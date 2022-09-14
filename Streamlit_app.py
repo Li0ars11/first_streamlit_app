163489fdc981bc2c56ddbf84bd487b91f8aef655
Li0ars11 <112098498+Li0ars11@users.noreply.github.com>
2022-09-14 10:59:37 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -47,7 +47,7 @@ streamlit.stop()
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
-    with my_cur = my_cnx.cursor() as my_cur:
+    with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
     return my_cur.fetchall()
    
